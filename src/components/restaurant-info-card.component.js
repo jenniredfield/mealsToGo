d91667d666f4804213b2836ade7ace6e483e14af
Jennifer Bird <jenniredfield@hotmail.com>
2022-04-06 11:03:04 -0300

    Finished 121

--- src/components/restaurant-info-card.component.js
+++ src/components/restaurant-info-card.component.js
@@ -4,6 +4,7 @@ import { Text } from "react-native";
 import styled from "styled-components";
 import { SvgXml } from "react-native-svg";
 import star from "../../assets/star";
+import open from "../../assets/open";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -36,17 +37,40 @@ const Row = styled.View`
   padding: ${(props) => props.theme.space[3]} 0;
 `;
 
+const Section = styled.View`
+  flex-direction: row;
+`;
+
+const SectionEnd = styled(Section)`
+  justify-content: flex-end;
+  flex: 1;
+`;
+
+const Open = styled(SvgXml)`
+  margin-left: 20px;
+`;
+
+const ClosedText = styled.Text`
+  color: ${(props) => props.theme.colors.text.error};
+  align-items: center;
+  justify-content: center;
+`;
+
+const IconImage = styled.Image`
+  margin-left: 20px;
+`;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some restaurant",
-    icon = "",
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://media.istockphoto.com/photos/delicious-meal-picture-id1295387240",
     ],
     address = "100 some random street",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily = false,
+    isClosedTemporarily = true,
   } = restaurant;
 
   const ratingArray = Array.from(new Array(Math.floor(rating)));
@@ -57,9 +81,22 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       <Info>
         <Title>{name}</Title>
         <Row>
-          {ratingArray.map(() => (
-            <SvgXml width="20" height="20" xml={star} />
-          ))}
+          <Section>
+            {ratingArray.map((_, i) => (
+              <SvgXml width="20" height="20" xml={star} key={`Star_${i}`} />
+            ))}
+          </Section>
+          <SectionEnd>
+            {isClosedTemporarily && <ClosedText>CLOSED TEMPORARILY</ClosedText>}
+            {isOpenNow ? <Open width="20" height="20" xml={open} /> : null}
+            <IconImage
+              source={{
+                uri: icon,
+                width: 20,
+                height: 20,
+              }}
+            />
+          </SectionEnd>
         </Row>
         <Address>{address}</Address>
       </Info>
